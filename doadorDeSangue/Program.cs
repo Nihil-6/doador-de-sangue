13d90359001ba75ab49812b410fa45c4599b2e64
Davi Xavier <davi.xa.oliveira@gmail.com>
2026-05-15 20:07:56 -0300

    vai validar se a pessoa pode doar sangue de acordo com a idade

---
+++ doadorDeSangue/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("pesso que escreva sua idade para confirmar a doação de sangue");
+
+int idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade >= 18 && idade <= 67)
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Doação de sangue confirmada");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("sua doação de sangue foi recusada, idade invalida");
+}
+Console.ResetColor();
